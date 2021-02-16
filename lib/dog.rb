ba6af29d28b3597af9aa96b33269a31218b8f3a5
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-02-15 22:20:59 -0800

    commit tree 2be9dae9b4a5a2c8007d850010797bbb72da27cb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,31 @@
-class Dog
-  # code goes here
+class Dog 
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+  @pets = []
+
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def pets=(cats,dogs)
+    @pets << self.cats
+    @pets << self.dogs
+  end
+
+  def list_pets
+    if @pets.count > 0
+      @pets
+    end
+  end
+
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
 class Owner
-  # code goes here
+
+  attr_accessor :dog, :cat, :pets
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def name 
+    @name
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    Owner.all.count
+  end
+
+  def self.reset_all
+    Owner.all.clear
+  end
+
+  def cats
+    Cat.all.select{|cat|cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|dog|dog.owner == self}
+  end
+
+  def buy_cat(new_cat)
+    Cat.new(new_cat, self)
+    self.cats.count
+  end
+
+  def buy_dog(new_dog)
+    Dog.new(new_dog, self)
+    self.dogs.count
+  end
+
+  def walk_dogs
+    self.dogs.each{|dog|dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each{|cat|cat.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
